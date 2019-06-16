4a4045fd0c368cd02d39a3bb082093874fee5c8e
Belinda Lei <belinda.lei95@gmail.com>
2019-06-16 20:29:03 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array 
+  my_new_array = []
+  return my_new_array 
+end 
+
+def array_with_two_elements 
+  my_two_array = ["apple", "banana"]
+end 
+
+def first_element(array)
+  array.first 
+end
+
+def third_element(array)
+  array[3]
+end
+
+
+
