bd25defcf43325df950ca07633510ec53a44b682
Belinda Lei <belinda.lei95@gmail.com>
2019-06-16 20:39:57 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  my_new_array = []
+  return my_new_array
+end
+
+def array_with_two_elements
+  my_two_array = ["apple", "banana"]
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  newArray = array[2]
+  return newArray
+end
+
+def last_element(array)
+  newArray = array.last
+  return newArray
+end
+
+def first_element_with_array_methods(array)
+  newArray = array.first
+  return newArray
+end
+
+def last_element_with_array_methods(array)
+  newArray = array.last
+  return newArray
+end
+
+def length_of_array(array)
+  newArray = array.length
+  return newArray
+end
